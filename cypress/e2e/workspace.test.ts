6a2d2fecfb512bf73fa84beb63a03f92af515a55
Teale Fristoe <fristoe@gmail.com>
2024-09-16 06:53:11 -0700

    Copy files from starter project.

---
+++ cypress/e2e/workspace.test.ts
@@ -0,0 +1,13 @@
+import { AppElements as ae } from "../support/elements/app-elements";
+
+context("Test the overall app", () => {
+  beforeEach(() => {
+    cy.visit("");
+  });
+
+  describe("Desktop functionalities", () => {
+    it("renders with text", () => {
+      ae.getApp().should("have.text", "Hello World");
+    });
+  });
+});
