020eecf7abafe13155df0627e23b1efaeb7d8fd2
Teale Fristoe <fristoe@gmail.com>
2025-02-12 14:20:11 -0800

    Remove barRef.

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -2,17 +2,15 @@
  * This component displays a progress bar
  */
 
-import React, { useRef } from "react";
+import React from "react";
 
 interface IProgressBarProps {
   percentComplete: number
 }
 export function ProgressBar({ percentComplete }: IProgressBarProps) {
-  const barRef = useRef<HTMLDivElement>(null);
-
   return (
     <div className='sq-progress-bar-container'>
-      <div className='sq-progress-bar' style={{ width: `${percentComplete}%` }} ref={barRef}>
+      <div className='sq-progress-bar' style={{ width: `${percentComplete}%` }}>
         <p className='sq-pb-label'>{percentComplete}%</p>
       </div>
     </div>
