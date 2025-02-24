2f3c9c711e945f8d2167823180a45c960bf8f0d8
Teale Fristoe <fristoe@gmail.com>
2025-02-24 12:05:38 -0800

    Remove redundant jest test.

--- src/storyq.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Storyq from './components/storyq';
-
-it('renders without crashing', async () => {
-  render(<Storyq />)
-  expect(screen.getByText("Welcome to StoryQ!")).toBeInTheDocument()
-});
