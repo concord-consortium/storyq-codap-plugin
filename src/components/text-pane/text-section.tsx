effecdbf8529ea54a535a12a7e5c22ccf367470f
Teale Fristoe <fristoe@gmail.com>
2025-03-04 10:29:32 -0800

    Turn entire section title into a button.

--- src/components/text-pane/text-section.tsx
+++ src/components/text-pane/text-section.tsx
@@ -20,12 +20,16 @@ function TextSectionTitle({ count, title }: ITextSectionTitleProps) {
   return (
     <span className="actual-title">
       {actual && (
-        <span>
-          True label: <span className="label" style={{ color }}>{actual}</span>
+        <>
+          <span>True label: </span><span className="label" style={{ color }}>{actual}</span>
           {predicted && <span>,&nbsp;</span>}
-        </span>
+        </>
       )}
-      {predicted && <span>Predicted label: <span className="label" style={{ color }}>{predicted}</span></span>}
+      {predicted && (
+        <>
+          <span>Predicted label: </span><span className="label" style={{ color }}>{predicted}</span>
+        </>
+      )}
       <span>&nbsp;</span>
       <span className="case-count">{`(${count} case${count === 1 ? "" : "s"})`}</span>
     </span>
@@ -41,15 +45,16 @@ export function TextSection({ textHeight, textSection }: ITextSectionProps) {
   const height = textSectionTitleHeight + (!hidden ? textHeight : 0);
   return (
     <div className="text-section" style={{ height }}>
-      <div className="text-section-title" style={{ height: textSectionTitleHeight }}>
+      <button
+        className="text-section-title"
+        onClick={() => textStore.toggleTextSectionVisibility(textSection)}
+        style={{ height: textSectionTitleHeight }}
+      >
         <TextSectionTitle count={text.length} title={title} />
-        <button
-          className={clsx("hide-button", { hidden })}
-          onClick={() => textStore.toggleTextSectionVisibility(textSection)}
-        >
+        <div className={clsx("hide-icon", { hidden })}>
           <CollapseExpandIcon />
-        </button>
-      </div>
+        </div>
+      </button>
       {!hidden && (
         <div className="text-section-text" style={{ height: textHeight }}>
           {text.map(textSectionText => {
