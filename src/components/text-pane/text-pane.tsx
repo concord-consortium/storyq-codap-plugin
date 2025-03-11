94cb7d6a02c933a34d963d0372e0bec611ce9fad
Teale Fristoe <fristoe@gmail.com>
2025-03-11 10:29:30 -0700

    Make textPaneTitle a view on domainStore.

--- src/components/text-pane/text-pane.tsx
+++ src/components/text-pane/text-pane.tsx
@@ -2,10 +2,8 @@ import React, { useEffect } from "react";
 import { reaction } from "mobx";
 import { observer } from "mobx-react";
 import { textFeedbackManager } from "../../managers/text_feedback_manager";
+import { domainStore } from "../../stores/domain_store";
 import { featureStore } from "../../stores/feature_store";
-import { targetDatasetStore } from "../../stores/target_dataset_store";
-import { targetStore } from "../../stores/target_store";
-import { testingStore } from "../../stores/testing_store";
 import { textStore } from "../../stores/text_store";
 import { TextParts } from "./text-parts";
 import { TextSection, textSectionTitleHeight } from "./text-section";
@@ -26,25 +24,6 @@ export const TextPane = observer(function TextPane() {
     );
   }, []);
 
-  // Update the title when the target dataset, title, or attribute changes
-  useEffect(() => {
-    return reaction(
-      () => {
-        return [
-          textStore.titleDataset, targetDatasetStore.targetDatasetInfo.title, targetStore.targetAttributeName,
-          testingStore.testingDatasetInfo.title, testingStore.testingAttributeName
-        ];
-      },
-      () => {
-        const dataset = textStore.titleDataset === "target"
-          ? targetDatasetStore.targetDatasetInfo.title : testingStore.testingDatasetInfo.title;
-        const attribute = textStore.titleDataset === "target"
-          ? targetStore.targetAttributeName : testingStore.testingAttributeName;
-        textStore.updateTitle(dataset, attribute);
-      }
-    );
-  }, []);
-
   const visibleTextSectionCount = textStore.textSections.filter(textSection => !textSection.hidden).length;
   const textHeight = containerHeight - textStore.textSections.length * textSectionTitleHeight;
   const sectionHeight = textHeight / visibleTextSectionCount;
@@ -52,7 +31,7 @@ export const TextPane = observer(function TextPane() {
   return (
     <div className="text-pane" style={{ height: paneHeight }}>
       <div className="text-title" style={{ height: titleHeight }}>
-        <TextParts textParts={textStore.textComponentTitle} />
+        <TextParts textParts={domainStore.textPaneTitle} />
       </div>
       <div className="text-container" style={{ height: containerHeight }}>
         {textStore.textSections.map(textSection => (
--- src/stores/text_store.ts
+++ src/stores/text_store.ts
@@ -4,8 +4,7 @@
  */
 
 import { makeAutoObservable } from 'mobx'
-import pluralize from "pluralize";
-import { ITextPart, ITextSection } from './store_types_and_constants';
+import { ITextSection } from './store_types_and_constants';
 
 type TitleDataset = "target" | "testing";
 
@@ -14,34 +13,11 @@ export interface ITextStoreJSON {
 }
 
 export class TextStore {
-  textComponentTitle: ITextPart[] = [];
   textSections: ITextSection[] = [];
   titleDataset: TitleDataset = "target";
 
   constructor() {
     makeAutoObservable(this, {}, { autoBind: true });
-  }
-
-  asJSON() {
-    return {
-      textComponentTitle: JSON.stringify(this.textComponentTitle)
-    };
-  }
-
-  fromJSON(json: ITextStoreJSON) {
-    if (json) {
-			try {
-				const title = JSON.parse(json.textComponentTitle);
-				this.textComponentTitle = title || [];
-			} catch (e) {
-				// The title used to be a simple string, so we have to accommodate that.
-				this.textComponentTitle = [{ text: json.textComponentTitle || "" }];
-			}
-    }
-  }
-
-  setTextComponentTitle(title: ITextPart[]) {
-    this.textComponentTitle = title;
   }
 
   setTitleDataset(titleDataset: TitleDataset) {
@@ -58,19 +34,6 @@ export class TextStore {
 
   toggleTextSectionVisibility(textSection: ITextSection) {
     textSection.hidden = !textSection.hidden;
-  }
-
-  updateTitle(datasetName: string, attributeName: string) {
-    if (datasetName && attributeName) {
-      this.textComponentTitle = [
-				{ text: `Selected ` },
-				{ text: pluralize(attributeName), classNames: ["highlighted"] },
-				{ text: " in " },
-				{ text: datasetName, classNames: ["highlighted"] }
-			];
-    } else {
-      this.textComponentTitle = [{ text: `Choose Data And Text To Begin` }];
-    }
   }
 
   async clearText() {
